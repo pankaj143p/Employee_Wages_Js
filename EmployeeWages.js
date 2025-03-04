f8ce5c19c5598781f25d3e354ec65ba896f08662
pankaj143p <pankaj114477pankaj@gmail.com>
2025-03-04 15:11:21 +0530

    [Pankaj] : Created function to get work hours.

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -1,2 +1,36 @@
+//uc1
 const isPresent = Math.random() < 0.5 ? "Absent" : "Present";
+//uc2
+const getDailyWage = () => {
+  let empCheck = Math.floor(Math.random() * 3);
+  let empHours = 0;
+  switch (empCheck) {
+    case 1:
+      empHours = 4;
+      break;
+    case 2:
+      empHours = 8;
+      break;
+    default:
+      empHours = 0;
+  }
+  return empHours * 20;
+};
+//uc3
+const getWorkHours = () => {
+  let empCheck = Math.floor(Math.random() * 3);
+  switch (empCheck) {
+    case 1:
+      return 4;
+    case 2:
+      return 8;
+    default:
+      return 0;
+  }
+};
+//uc1
 console.log(isPresent);
+// uc2
+console.log(getDailyWage());
+//uc3
+console.log(getWorkHours());

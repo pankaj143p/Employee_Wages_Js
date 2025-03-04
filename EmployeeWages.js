f0d62f82d87f523e0142bd8fe0c69f3914c42f3c
pankaj143p <pankaj114477pankaj@gmail.com>
2025-03-04 15:29:56 +0530

    [Pankaj] : validate employee data added.

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -145,6 +145,22 @@ const validateName = (name) => {
     return error;
   }
 };
+//uc14
+const validateEmployeeData = (id, salary, gender, date) => {
+  const idPattern = /^[1-9][0-9]*$/;
+  const salaryPattern = /^[1-9][0-9]*$/;
+  const genderPattern = /^(M|F)$/;
+  const datePattern = /^(0[1-9]|1[0-9]|2[0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+  try {
+    if (!idPattern.test(id)) throw "Invalid ID";
+    if (!salaryPattern.test(salary)) throw "Invalid salary";
+    if (!genderPattern.test(gender)) throw "Invalid gender";
+    if (new Date(date) > new Date()) throw "Invalid date";
+    return true;
+  } catch (error) {
+    return error;
+  }
+};
 //uc1
 console.log(isPresent);
 // uc2
@@ -174,3 +190,5 @@ console.log(workDayOperations());
 console.log(employeeData);
 //uc13
 console.log(validateName("Sweta"));
+//uc14
+console.log(validateEmployeeData(1, 3000, "M", "01/03/2025"));
